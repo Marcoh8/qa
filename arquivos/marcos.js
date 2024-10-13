1e26f55417c4f6f23093f0aad35d6ff5311a6761
Marcoh8 <marcoscruzeirodosul78@gmail.com>
2024-10-13 11:49:02 -0300

    outro teste

--- arquivos/marcos.js
+++ arquivos/marcos.js
@@ -1,1 +1,3 @@
-alert("Ola mundo! Esse é meu projeto. futuras atualizações serão feitas e possíveis bugs encontrados e corrigidos ")
+alert("Ola mundo! Esse é meu projeto.")
+alert("Futuras atualizações serão feitas e possíveis Erros, Falhas e Defeitos quando encontrados corrigidos ")
+alert(" Entre em contato para novas observações")
